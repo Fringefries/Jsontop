3ce4294f0eedc2b947bd57323d5065559040a26d
Fringefries <alegrepatrick9098@gmail.com>
2025-07-13 18:04:08 +0800

    time clock

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-const date =  new Date();
+function clock(){
+    const now = new Date();
+    let hrs = now.getHours();
+    const meridiem = hrs >= 12 ? "PM" : "AM";
+    hrs = hrs % 12 || 12;
+    hrs = hrs.toString().padStart(2, 0);
 
-console.log(date);
+    const min = now.getMinutes().toString().padStart(2, 0);
+    const sec = now.getSeconds().toString().padStart(2, 0);
+    const timestring = `${hrs} : ${min} : ${sec} `;
+    document.getElementById('orasan').textContent = timestring;
+
+}
+clock();
+
+setInterval(clock , 100);
